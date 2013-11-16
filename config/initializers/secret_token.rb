8955a99a13842bf701efc3ef73e42e725c244785
Amar <amar_cs@live.nl>
2013-11-16 14:09:56 +0100

    first deposit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Workhours::Application.config.secret_token = 'a2e84cbd019309df23924c73ee08d2bb045b58c98c58e69be84b986bc010b2491d0c391a99f3ac5ac37aad297ba24651d61488fec8192f1bf743dd91e569120a'
