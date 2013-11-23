cfb96a79eb3e63755e00bdfc4c3d462b040f06a0
Amar <amar_cs@live.nl>
2013-11-23 15:18:25 +0100

    associations toegevoegd

--- app/models/workhour.rb
+++ app/models/workhour.rb
@@ -1,5 +1,13 @@
 class Workhour < ActiveRecord::Base
-  attr_accessible :user_id, :date_of_workhour, :hours, :factor, :net_wage
-  
+  attr_accessible :user_id, :date_of_workhour, :hours, :factor, :net_wage, :user
   belongs_to :user
+  
+  validates_presence_of :user
+  
+  before_save :net_wage_from_user
+  
+  def net_wage_from_user
+   net_wage = user.hourly_net_wage
+    
+  end
 end
